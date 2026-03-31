7bdd53457caa11bbb1daee6b1f67ad5bbef35476
yujimin413 <84549242+yujimin413@users.noreply.github.com>
2026-03-31 14:00:50 +0900

    refactor(widget): widgetShortcuts defaultQuery 및 getWidgetDefaultQuery 추가

--- src/config/widgetShortcuts.js
+++ src/config/widgetShortcuts.js
@@ -16,6 +16,7 @@ const WIDGET_SHORTCUTS = [
     keywords: ['잔고', '계좌', '자산', '평가자산', '내계좌', '예수금', '투자자산', '계좌현황'],
     widgetTypeId: 'balance',
     Icon: Wallet,
+    defaultQuery: '내 계좌 잔고 알려줘',
   },
   {
     key: 'exchange',
@@ -23,6 +24,7 @@ const WIDGET_SHORTCUTS = [
     keywords: ['환율', '달러', '엔', '유로', 'usd', 'jpy', 'eur', '원달러', '환전', '외화', '파운드', '위안'],
     widgetTypeId: 'exchange',
     Icon: ArrowLeftRight,
+    defaultQuery: '현재 환율 알려줘',
   },
   {
     key: 'ranking',
@@ -30,6 +32,7 @@ const WIDGET_SHORTCUTS = [
     keywords: ['순위', '랭킹', '거래대금', '상승률', '거래량', '급등', '상한가', '인기종목', '하락률'],
     widgetTypeId: 'ranking',
     Icon: TrendingUp,
+    defaultQuery: '실시간 종목 순위 알려줘',
   },
   {
     key: 'index',
@@ -37,6 +40,7 @@ const WIDGET_SHORTCUTS = [
     keywords: ['지수', '코스피', '코스닥', '나스닥', 'kospi', 'kosdaq', 'nasdaq', '다우', 's&p', '에센피', '뉴욕', '증시'],
     widgetTypeId: 'index',
     Icon: BarChart2,
+    defaultQuery: '주요 지수 알려줘',
   },
   {
     key: 'market-overview',
@@ -44,6 +48,7 @@ const WIDGET_SHORTCUTS = [
     keywords: ['시황', '시장', '헤드라인', '뉴스', '증시뉴스', '장세', '오늘증시', '코스피', '코스닥', '나스닥', '에센피', 's&p', 'nasdaq', 'kospi', 'kosdaq', '다우', '뉴욕'],
     widgetTypeId: 'market-overview',
     Icon: Newspaper,
+    defaultQuery: '오늘의 시황 알려줘',
   },
   {
     key: 'portfolio',
@@ -51,6 +56,7 @@ const WIDGET_SHORTCUTS = [
     keywords: ['포트폴리오', '포트', '비중', '보유종목', '보유주식', '내주식', '수익률', '손익', '평가손익', '자산배분'],
     widgetTypeId: 'portfolio',
     Icon: PieChart,
+    defaultQuery: '내 포트폴리오 알려줘',
   },
   {
     key: 'watchlist',
@@ -58,6 +64,7 @@ const WIDGET_SHORTCUTS = [
     keywords: ['관심', '관심종목', '즐겨찾기', '찜', '위시'],
     widgetTypeId: 'watchlist',
     Icon: Heart,
+    defaultQuery: '내 관심종목 알려줘',
   },
   {
     key: 'trade-history',
@@ -65,7 +72,16 @@ const WIDGET_SHORTCUTS = [
     keywords: ['거래내역', '매매', '주문내역', '체결', '내역', '거래기록', '주문', '체결내역', '매수내역', '매도내역'],
     widgetTypeId: 'trade-history',
     Icon: History,
+    defaultQuery: '거래내역 알려줘',
   },
 ]
 
 export default WIDGET_SHORTCUTS
+
+export function getWidgetDefaultQuery(widgetTypeId, config = {}) {
+  if (widgetTypeId === 'stock-chart') {
+    const name = config.stockName ?? null
+    return name ? `${name} 주가 알려줘` : '주가 차트 보여줘'
+  }
+  return WIDGET_SHORTCUTS.find((s) => s.widgetTypeId === widgetTypeId)?.defaultQuery ?? null
+}
